c0c6b91ab5fcce916ee20f3efa2a68a042d87689
Nahuel Garcia <nhlgrc@hotmail.com>
2025-12-26 16:12:05 -0300

    desde casa

---
+++ lib/presentation/widgets/share/message_field_box.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40)
+    );
+
+    return TextFormField(
+      decoration: InputDecoration(
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.send_outlined),
+        ),
+      ),
+    );
+  }
+}
